8cf6cfe1d1ecd9a6e9456ff1533912b4968a6de7
Mick-Web-dev <mickael.hesliere@gmail.com>
2022-02-23 08:10:04 +0100

    Test d'affichage du formulaire contact valide, ajustement de l'affichage et de la validation du champ imput->pseudo via l'attribut pattern

--- php/result.php
+++ php/result.php
@@ -40,7 +40,37 @@ if (isset($form) && ($form == 1)) {
 */
 
 function affForm($form){
-    
-}
+    // condition 1- si l'utilisateur valide sans selectionner un formulaire il est redirigé sur l'index
+    //et on lui retourne un message
+    if (isset($form) && ($form == 0)) {
+        echo 'Vous devez choisir un formulaire !';
+        header('Location:../index.php');
+    }//test=ok
+
+    //Formulaire-1
+    if (isset($form) && ($form == 1)) {
+        echo '
+        <div class="containerForm">
+            <div id="register" class="containerForm1">
+                <h1>Formulaire de contact</h1>
+                <p>Merci de renseigner les champs suivant afin de vous enregistrer.</p>
+                <form method="POST" action="#">
+                    <label for="pseudo">Votre speudo :</label>
+                    <input type="text" id="pseudo" name="pseudo" required pattern="[a-zA-Z0-9]{4,8}">       
+                    <br><br>
+                    <label for="mail">Votre mail :</label>
+                    <input type="email" id="mail" name="mail"required>
+                    <br><br>
+                    <label for="mp">Votre mot de passe :</label>
+                    <input type="password" id="mp" name="mp" required>
+                    <br><br>
+                    <input type="submit" value="Soumettre" class="btn btn-primary">
+                </form>
+            </div>
+        </div>
+        ';     
+    }//test formulaire-1 ok
+     
+} return affForm($form);
 
 require 'footer.php';
